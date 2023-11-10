cc6faa9e5314d0dbe2da054960174b7e5cf2fbcc
Jair Gino <jairgino@hotmail.com>
2023-11-09 23:18:03 -0300

    Chore: update Navbar and Footer content components

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,26 +1,32 @@
-import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import { GitlabLogo, GithubLogo, LinkedinLogo } from '@phosphor-icons/react'
 
 function Footer() {
 
-    let data = new Date().getFullYear()
+  let currentYear = new Date().getFullYear()
 
-    return (
-        <>
-            <div className="flex justify-center bg-indigo-900 text-white">
-                <div className="container flex flex-col items-center py-4">
-                    <p className='text-xl font-bold'>
-                            Blog Pessoal Generation | Copyright: {data}
-                        </p>
-                    <p className='text-lg'>Acesse nossas redes sociais</p>
-                    <div className='flex gap-2'>
-                        <LinkedinLogo size={48} weight='bold' />
-                        <InstagramLogo size={48} weight='bold' />
-                        <FacebookLogo size={48} weight='bold' />
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="flex justify-center bg-indigo-900 text-white">
+        <div className="container flex flex-col items-center py-4">
+          <p className='text-xl font-bold'>
+            Blog Pessoal Generation | Copyright: {currentYear}
+          </p>
+          <p className='text-lg'>Acesse nossas redes sociais</p>
+          <div className='flex gap-2'>
+            <a target='_blank' href="https://www.linkedin.com/in/jair-gino-64b408264">
+              <LinkedinLogo size={48} />
+            </a>
+            <a target='_blank' href="https://github.com/JairGino/">
+              <GithubLogo size={48} />
+            </a>
+            <a target='_blank' href="https://github.com/JairGino/">
+              <GitlabLogo size={48} />
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Footer
